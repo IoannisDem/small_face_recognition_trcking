7e3ec5976799f258c9faea2206ce1070211de783
PJ00 <ioannisdem2000@gmail.com>
2024-06-11 19:56:22 +0100

    usign embeddings

--- utils/custom_model.py
+++ utils/custom_model.py
@@ -4,16 +4,11 @@ import torchvision
 from torchvision import models
 
 class distill_model(nn.Module):
-    def __init__(self, output_class):
+    def __init__(self, output_embed=512):
         super(distill_model, self).__init__()
         self.model = models.mobilenet_v3_small(pretrained=True)
         
-        new_classifier = nn.Sequential(
-            nn.Linear(576, 1024, bias=True),
-            nn.Hardswish(), 
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(1024, output_class)
-        )
+        new_classifier = nn.Linear(576, output_embed, bias=True)
         
         self.model.classifier = new_classifier
         
